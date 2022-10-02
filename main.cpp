0fbaf438d3cde4ea6fcc2425bdad992733554cec
FauziIzza <109806731+FauziIzza@users.noreply.github.com>
2022-10-02 22:20:31 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int r;
+    float phi=3.14,luas,keliling;
+
+    cout<<"masukan jari jari lingkaran : ";
+    cin>>r;
+
+    luas=phi*r*r;
+    keliling=2*phi*r;
+    cout<<"luas lingkaran adalah "<<luas ;
+        cout<<"\nluas lingkaran adalah "<<keliling ;
+
+    return 0;
+}
